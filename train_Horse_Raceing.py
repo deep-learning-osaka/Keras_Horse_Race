50a8fb2da31c19973c00590f12503b01f1309e00
山木緑郎 <yamakirokurou@MacBook-Pro-of-Rock.local>
2018-03-31 19:31:09 +0900

    callbackを実装しました

--- train_Horse_Raceing.py
+++ train_Horse_Raceing.py
@@ -4,7 +4,8 @@ import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import RMSprop
+from keras.optimizers import SGD
+from keras.callbacks import TensorBoard
 
 import numpy as np
 
@@ -12,10 +13,10 @@ batch_size = 10
 num_classes = 18
 epochs = 20
 
-x_train = np.loadtxt("data/x_train.csv",delimiter=",")
-y_train = np.loadtxt("data/y_train.csv",delimiter=",")
-x_test = np.loadtxt("data/x_train.csv",delimiter=",")
-y_test = np.loadtxt("data/y_train.csv",delimiter=",")
+x_train = np.loadtxt("sample_train_data/x_train.csv",delimiter=",")
+y_train = np.loadtxt("sample_train_data/y_train.csv",delimiter=",")
+x_test = np.loadtxt("sample_train_data/x_train.csv",delimiter=",")
+y_test = np.loadtxt("sample_train_data/y_train.csv",delimiter=",")
 
 
 x_train = x_train.astype('float32')
@@ -33,16 +34,26 @@ model.add(Dense(num_classes, activation='softmax'))
 
 model.summary()
 
-sgd=keras.optimizers.SGD(lr=1e-4, momentum=0.9)
 model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
+              optimizer=keras.optimizers.SGD(lr=1e-4, momentum=0.9),
               metrics=['accuracy'])
+
+callbacks = [keras.callbacks.ModelCheckpoint('./checkpoints/weights_epoch:{epoch:03d}_valloss:{val_loss:.3f}_valacc:{val_acc:.3f}.hdf5',
+                                verbose=1,
+                                save_best_only=True,
+                                mode='auto',
+                                save_weights_only=True,
+                                period=3),
+             keras.callbacks.EarlyStopping(monitor='val_acc', min_delta=0.01, patience=25, verbose=1, mode='auto'),
+             TensorBoard(log_dir='./graph', histogram_freq=0, write_graph=True, write_grads=True, write_images=True)]
 
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
                     verbose=1,
-                    validation_data=(x_test, y_test))
+                    validation_data=(x_test, y_test),
+                    callbacks=callbacks)
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
